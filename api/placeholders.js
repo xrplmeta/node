191f1633e77930e2b7c4292502098c7ee862ec9a
Mister Worldwide <mworldwide@protonmail.com>
2021-10-17 17:46:05 +0200

    updates

--- api/placeholders.js
+++ api/placeholders.js
@@ -1,5 +1,14 @@
 export default {
-	assets: [
+	currencies: [
+		{
+			currency: 'USD',
+			issuer: 'rvYAfWj5gh67oV6fW32ZzP3Aw4Eubs59B',
+			meta: {
+				class: 'fiat',
+				currencyName: 'U.S. Dollar',
+				issuerName: 'Bitstamp'
+			}
+		},
 		{
 			currency: 'USD',
 			issuer: 'rhub8VRN55s94qWKDv6jmDy1pUykJzF3wq',
@@ -30,6 +39,17 @@ export default {
 			}
 		},
 		{
+			currency: 'BTC',
+			issuer: 'rchGBxcD1A1C2tdxF6papQYZ8kjRKMYcL',
+			meta: {
+				class: 'crypto',
+				currencyName: 'Bitcoin',
+				currencyIcon: 'https://cryptologos.cc/logos/bitcoin-btc-logo.svg',
+				currencyColor: '#f7931a',
+				issuerName: 'GateHub Fifth'
+			}
+		},
+		{
 			currency: 'ETH',
 			issuer: 'rcA8X3TVMST1n3CJeAdGk1RdRCHii7N2h',
 			meta: {
@@ -50,6 +70,39 @@ export default {
 				currencyColor: '#008CE7',
 				issuerName: 'Gatehub Fifth'
 			}
+		},
+		{
+			currency: 'LTC',
+			issuer: 'rcRzGWq6Ng3jeYhqnmM4zcWcUh69hrQ8V',
+			meta: {
+				class: 'crypto',
+				currencyName: 'Litecoin',
+				currencyIcon: 'https://cryptologos.cc/logos/history/litecoin-logo-2013.png',
+				currencyColor: '#dddddd',
+				issuerName: 'Gatehub Fifth'
+			}
+		},
+		{
+			currency: 'BCH',
+			issuer: 'rcyS4CeCZVYvTiKcxj6Sx32ibKwcDHLds',
+			meta: {
+				class: 'crypto',
+				currencyName: 'Bitcoin Cash',
+				currencyIcon: 'https://cryptologos.cc/logos/bitcoin-cash-bch-logo.svg',
+				currencyColor: '#0AC18E',
+				issuerName: 'Gatehub Fifth'
+			}
+		},
+		{
+			currency: 'XAU',
+			issuer: 'rcoef87SYMJ58NAFx7fNM5frVknmvHsvJ',
+			meta: {
+				class: 'commodity',
+				currencyName: 'Gold',
+				currencyColor: '#0AC18E',
+				issuerName: 'Gatehub Fifth'
+			}
 		}
 	]
 }
+
