8da80e0aa683c727b78d07d3245766a0d60b7ad0
Mwni <manuel@tigmar.de>
2022-01-04 14:27:06 +0100

    mig

--- migration/v3.js
+++ migration/v3.js
@@ -10,5 +10,8 @@ let repo = initRepo({
 
 
 repo.tx(() => {
-	
+	repo.exec(`ALTER TABLE "main"."Stats" RENAME COLUMN "count" TO "trustlines"`)
+	repo.exec(`ALTER TABLE "main"."Balances" RENAME COLUMN "trustline" TO "token"`)
+	repo.exec(`ALTER TABLE "main"."States" RENAME COLUMN "currencies" TO "tokens"`)
+	repo.exec(`ALTER TABLE "main"."Trustlines" RENAME TO "Tokens"`)
 })
