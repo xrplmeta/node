489ccded6c18bdf1b43e1bcb438ac095ae40dab0
Mwni <manuel@tigmar.de>
2022-05-03 14:22:45 +0200

    added snapshot NFToken support

--- snapshot/model.js
+++ snapshot/model.js
@@ -2,6 +2,7 @@ import { Client } from '@structdb/sqlite'
 import { fromRippled as fromRippledAmount } from '@xrplworks/amount'
 import { rippleToUnix } from '@xrplworks/time'
 import XFL from '@xrplworks/xfl'
+import { encodeAccountID } from 'ripple-address-codec'
 import schemas from '../schemas/index.js'
 
 export default class Snapshot extends Client{
@@ -23,11 +24,17 @@ export default class Snapshot extends Client{
 
 			case 'Offer': 
 				return await this.addCurrencyOffer(entry)
+
+			case 'NFTokenPage':
+				return await this.addNFTokenPage(entry)
+
+			case 'NFTokenOffer':
+				return await this.addNFTokenOffer(entry)
 		}
 	}
 
 	async addAccountRoot(entry){
-		return await this.accounts.createOne({
+		await this.accounts.createOne({
 			data: {
 				address: entry.Account,
 				emailHash: entry.EmailHash,
@@ -40,7 +47,7 @@ export default class Snapshot extends Client{
 	}
 
 	async addRippleState(entry){
-		return await this.rippleStates.createOne({
+		await this.rippleStates.createOne({
 			data: {
 				currency: { code: entry.Balance.currency },
 				lowAccount: { address: entry.LowLimit.issuer },
@@ -54,7 +61,7 @@ export default class Snapshot extends Client{
 		let takerPays = fromRippledAmount(entry.TakerPays)
 		let takerGets = fromRippledAmount(entry.TakerGets)
 
-		return await this.currencyOffers.createOne({
+		await this.currencyOffers.createOne({
 			data: {
 				account: { address: entry.Account },
 				takerPaysCurrency: { code: takerPays.currency },
@@ -73,5 +80,26 @@ export default class Snapshot extends Client{
 					: null
 			}
 		})
+	}
+
+	async addNFTokenPage(entry){
+		let address = encodeAccountID(Buffer.from(entry.index.slice(0, 40), 'hex'))
+
+		for(let { NFToken } of entry.NFTokens){
+			let issuer = encodeAccountID(Buffer.from(NFToken.NFTokenID.slice(8, 48), 'hex'))
+
+			await this.nfTokens.createOne({
+				data: {
+					owner: { address },
+					issuer: { address: issuer },
+					tokenId: NFToken.NFTokenID,
+					uri: NFToken.URI
+				}
+			})
+		}
+	}
+
+	async addNFTokenOffer(entry){
+
 	}
 }
