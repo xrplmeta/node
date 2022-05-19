4e584749223dbc238cfe75340d2c1ec0a074eadc
Mwni <manuel@tigmar.de>
2022-05-19 16:50:43 +0200

    restructured app minimal

--- app/ledger.js
+++ app/ledger.js
@@ -20,49 +20,22 @@ export default async function(ctx){
 
 
 export async function createSnapshot(ctx){
-	let ledger = database.init({ ...ctx, variant: 'live' })
-
 	log.pipe(ctx.log)
+
+	let ledger = database.init({ ...ctx, variant: 'live' })
 
 	await snapshot.create({ ...ctx, ledger })
 	await ledger.fork({ variant: 'backfill' })
 }
 
-
-
-/*
-export const name = 'ledger.snapshot'
-
-export async function skip(ctx){
-	return ctx.args.only && !ctx.args.only.includes(name)
-}
-
-export async function start(ctx){
-	let snapshot = database.init({ ctx, variant: 'live' })
-
-	if(await snapshot.isIncomplete()){
-		await spawn(':createCheckpoint', ctx)
-	}
-
-	await spawn(':workLive', ctx)
-	await spawn(':workBackfill', ctx)
-}
-
-export async function createCheckpoint(ctx){
-	let snapshot = database.init({ ctx, variant: 'live' })
-
-	await checkpoint.create({ ctx, snapshot })
-	await snapshot.fork({ variant: 'backfill' })
-}
-
 export async function workLive(ctx){
-	let snapshot = database.init({ ctx, variant: 'live' })
+	log.pipe(ctx.log)
+	
+	let ledger = database.init({ ...ctx, variant: 'live' })
 
-	await live.work({ ctx, snapshot })
+	
 }
 
 export async function workBackfill(ctx){
-	let snapshot = database.init({ ctx, variant: 'backfill' })
-
-	await backfill.work({ ctx, snapshot })
-}*/
+	
+}
