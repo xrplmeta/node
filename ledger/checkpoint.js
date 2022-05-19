4c504b9feb4c964adac1e602632016d21480a7ab
Mwni <manuel@tigmar.de>
2022-05-19 22:34:43 +0200

    added basic checkpoint code

--- ledger/checkpoint.js
+++ ledger/checkpoint.js
@@ -1,6 +1,10 @@
 import log from '@mwni/log'
 
 
-export async function create({ config, xrpl, ledger }){
+export async function create({ config, ledger }){
+	let state = await ledger.getState()
+
+	log.info(`creating checkpoint at ledger #${state.ledgerIndex}`)
+
 	
-})
+}
