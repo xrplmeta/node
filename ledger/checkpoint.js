70304eaf297ac3a313c0e82cece6c0108b04a8df
Mwni <manuel@tigmar.de>
2022-05-20 05:18:25 +0200

    advanced checkpoint code

--- ledger/checkpoint.js
+++ ledger/checkpoint.js
@@ -6,5 +6,61 @@ export async function create({ config, ledger }){
 
 	log.info(`creating checkpoint at ledger #${state.ledgerIndex}`)
 
+	let issuedCurrencies = []
+
+	for(let side of ['low', 'high']){
+		let accountKey = `${side}Account`
+		let issuerKey = `${side}Issuer`
+
+		let partIssuedCurrencies = await ledger.rippleStates.readMany({
+			include: {
+				currency: true,
+				[accountKey]: true,
+			},
+			where: {
+				[issuerKey]: true,
+			},
+			distinct: ['currency', accountKey]
+		})
+
+		for(let row of partIssuedCurrencies){
+			let currency = row.currency.code
+			let issuer = row[accountKey].address
+
+			if(issuedCurrencies.some(ic => ic.currency === currency || ic.issuer === issuer))
+				continue
+
+			issuedCurrencies.push({ currency, issuer })
+		}
+	}
 	
+	log.info(`got`, issuedCurrencies.length, `issued currencies to walk through`)
+
+	for(let issuedCurrency of issuedCurrencies){
+		await processIssuedCurrency({ issuedCurrency, config, ledger })
+	}
+}
+
+
+async function processIssuedCurrency({ issuedCurrency, config, ledger }){
+	let { currency, issuer } = issuedCurrency
+
+	let rippleStates = await ledger.rippleStates.readMany({
+		where: {
+			OR: [
+				{
+					currency: { code: currency },
+					lowAccount: { address: issuer },
+					lowIssuer: true
+				},
+				{
+					currency: { code: currency },
+					highAccount: { address: issuer },
+					highIssuer: true
+				}
+			]
+		}
+	})
+
+	console.log(currency, rippleStates.length)
 }
