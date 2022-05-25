242133372673ac854f1ae240fd23ae4ad6adcb2b
Mwni <manuel@tigmar.de>
2022-05-25 17:59:17 +0200

    improved naming scheme

--- app/ledger.js
+++ app/ledger.js
@@ -1,44 +1,48 @@
 import log from '@mwni/log'
 import { spawn } from 'nanotasks'
-import { open as openLedgerDatabase } from '../ledger/database.js'
-import { open as openMetaDatabase } from '../meta/database.js'
-import * as snapshot from '../ledger/snapshot.js'
-import * as checkpoint from '../meta/checkpoint.js'
+import { openLedger, cloneLedger, openMeta } from '../db/index.js'
+import { isIncomplete } from '../ledger/state.js'
+import { create as createSnapshot } from '../ledger/snapshot.js'
+import { create as createCheckpoint } from '../meta/checkpoint.js'
 
 
 
 export default async function(ctx){
-	let ledger = openLedgerDatabase({ ...ctx, variant: 'live' })
+	let ledger = openLedger({ ...ctx, variant: 'live' })
 
-	if(await ledger.isIncomplete()){
-		await spawn(':createSnapshot', { ...ctx, log })
+	if(await isIncomplete({ ledger })){
+		await spawn(':runCreateSnapshot', { ...ctx, log })
 	}
 
-	await spawn(':workLive', { ...ctx, log })
-	await spawn(':workBackfill', { ...ctx, log })
+	await spawn(':runLive', { ...ctx, log })
+	await spawn(':runBackfill', { ...ctx, log })
 }
 
 
-export async function createSnapshot(ctx){
+export async function runCreateSnapshot(ctx){
 	log.pipe(ctx.log)
 
-	let ledger = openLedgerDatabase({ ...ctx, variant: 'live' })
+	let ledger = openLedger({ ...ctx, variant: 'live' })
 
-	await snapshot.create({ ...ctx, ledger })
-	await ledger.fork({ variant: 'backfill' })
+	log.info('creating snapshot now')
+	await createSnapshot({ ...ctx, ledger })
+
+	log.info('cloning ledger file for backfill...')
+	await cloneLedger({ ...ctx, ledger, newVariant: 'backfill' })
+	log.info('ledger file successfully cloned')
 }
 
-export async function workLive(ctx){
+export async function runLive(ctx){
 	log.pipe(ctx.log)
 	
-	let ledger = openLedgerDatabase({ ...ctx, variant: 'live' })
-	let meta = openMetaDatabase({ ...ctx })
+	let ledger = openLedger({ ...ctx, variant: 'live' })
+	let meta = openMeta({ ...ctx })
 
-	await checkpoint.create({ ...ctx, meta, ledger })
+	await createCheckpoint({ ...ctx, meta, ledger })
 
 	//await live.work({ ...ctx, ledger })
 }
 
-export async function workBackfill(ctx){
+export async function runBackfill(ctx){
 	
 }
