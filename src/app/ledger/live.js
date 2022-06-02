b90d0f37974cb9e28058c2cdd1b0ba7e6b5799d5
Mwni <manuel@tigmar.de>
2022-06-02 05:30:11 +0200

    implemented app live task with stream

--- src/app/ledger/live.js
+++ src/app/ledger/live.js
@@ -1,8 +1,31 @@
 import log from '@mwni/log'
+import { getCurrentIndex, isCheckpointDue } from '../../lib/snapshot/state.js'
+import { open as openMetaStore } from '../../store/meta.js'
+import { open as openSnapshotStore } from '../../store/snapshot.js'
+import { start as startStream } from '../../lib/ledger/stream.js'
+import { create as createCheckpoint } from '../../lib/meta/checkpoint.js'
+import { extract as extractLedgerMeta } from '../../lib/meta/ledgers.js'
+import { extract as extractTokenExchanges } from '../../lib/meta/token/exchanges.js'
 
 export async function run(ctx){
 	if(ctx.log)
 		log.pipe(ctx.log)
 
 	
+	let meta = await openMetaStore(ctx)
+	let snapshot = await openSnapshotStore({ ...ctx, variant: 'live' })
+	let startLedgerIndex = await getCurrentIndex({ snapshot })
+	let stream = await startStream({ ...ctx, direction: 'forward', startLedgerIndex })
+
+	while(true){
+		if(isCheckpointDue({ ...ctx, snapshot })){
+			await createCheckpoint({ ...ctx, snapshot, meta })
+		}
+
+		let ledger = await stream.next()
+
+		await extractLedgerMeta({ ledger, meta })
+		await extractTokenExchanges({ ledger, meta })
+		await advanceSnapshot({ ledger, snapshot })
+	}
 }
---
+++ src/lib/ledger/stream.js
@@ -0,0 +1,89 @@
+import log from '@mwni/log'
+import { rippleToUnix, wait } from '@xrplkit/time'
+
+const concurrency = 4
+
+export async function start({ config, xrpl, direction, startLedgerIndex }){
+	let strideIndex = direction === 'forward' ? 1 : 1
+	let currentIndex = startLedgerIndex
+	let targetIndex
+	let resolveNext
+	let queue = {}
+
+	if(direction === 'forward'){
+		let { result } = await xrpl.request({ 
+			command: 'ledger', 
+			ledger_index: 'validated' 
+		})
+
+		targetIndex = parseInt(result.ledger.ledger_index)
+		log.info(targetIndex - currentIndex, `ledgers behind`)
+	}else{
+		targetIndex = 0
+	}
+
+	for(let n=0; n<concurrency; n++){
+		(async () => {
+			let index = currentIndex
+
+			while(true){
+				let stepsToTarget = (targetIndex - index) * strideIndex
+	
+				if(stepsToTarget < 0){
+					await wait(100)
+					continue
+				}
+
+				if(Object.keys(queue).length > 1000){
+					await wait(1000)
+					continue
+				}
+
+				if(queue.hasOwnProperty(index)){
+					index += strideIndex
+					continue
+				}
+
+				queue[index] = undefined
+
+				try{
+					let { result } = await xrpl.request({ 
+						command: 'ledger', 
+						ledger_index: index,
+						transactions: true,
+						expand: true
+					})
+
+					queue[index] = {
+						index: index,
+						closeTime: rippleToUnix(result.ledger.close_time),
+						transactions: result.ledger.transactions
+					}
+
+					if(resolveNext)
+						resolveNext()
+						
+				}catch(error){
+					console.error(`failed to fetch ledger #${index}:`)
+					console.error(error)
+					delete queue[index]
+				}
+			}
+		})()
+	}
+
+	return {
+		async next(){
+			while(!queue[currentIndex]){
+				await new Promise(resolve => resolveNext = resolve)
+			}
+
+			let next = queue[currentIndex]
+
+			delete queue[currentIndex]
+			currentIndex++
+
+			return next
+		}
+	}
+}
