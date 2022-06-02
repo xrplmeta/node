9d4b35ce012efb0d22eafdfcabd978094f13103e
Mwni <manuel@tigmar.de>
2022-06-02 05:32:38 +0200

    added checkpoint due check

--- src/lib/snapshot/state.js
+++ src/lib/snapshot/state.js
@@ -11,3 +11,8 @@ export async function isIncomplete({ snapshot }){
 	let state = await getState({ snapshot })
 	return !state || state.snapshotMarker || state.entriesCount === 0
 }
+
+export async function isCheckpointDue({ snapshot, config }){
+	let state = await getState({ snapshot })
+	return state.ledgerIndex % config.ledger.checkpoint.everyNthLedger === 0
+}
