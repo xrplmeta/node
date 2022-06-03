3a9c45f702854ad15a50198e19461f8d32aa9357
Mwni <manuel@tigmar.de>
2022-06-03 17:34:07 +0200

    added live logging

--- src/app/ledger/live.js
+++ src/app/ledger/live.js
@@ -2,10 +2,12 @@ import log from '@mwni/log'
 import { getCurrentIndex, isCheckpointDue } from '../../lib/snapshot/state.js'
 import { open as openMetaStore } from '../../store/meta.js'
 import { open as openSnapshotStore } from '../../store/snapshot.js'
+import { advance as advanceSnapshot } from '../../lib/snapshot/diff.js'
 import { start as startStream } from '../../lib/ledger/stream.js'
 import { create as createCheckpoint } from '../../lib/meta/checkpoint.js'
 import { extract as extractLedgerMeta } from '../../lib/meta/ledgers.js'
 import { extract as extractTokenExchanges } from '../../lib/meta/token/exchanges.js'
+
 
 export async function run(ctx){
 	if(ctx.log)
@@ -18,7 +20,7 @@ export async function run(ctx){
 	let stream = await startStream({ ...ctx, direction: 'forward', startLedgerIndex })
 
 	while(true){
-		if(isCheckpointDue({ ...ctx, snapshot })){
+		if(await isCheckpointDue({ ...ctx, snapshot })){
 			await createCheckpoint({ ...ctx, snapshot, meta })
 		}
 
@@ -27,5 +29,10 @@ export async function run(ctx){
 		await extractLedgerMeta({ ledger, meta })
 		await extractTokenExchanges({ ledger, meta })
 		await advanceSnapshot({ ledger, snapshot })
+
+		log.accumulate.info({
+			line: [`advanced %advancedLedgers ledgers in %time`],
+			advancedLedgers: 1
+		})
 	}
 }
