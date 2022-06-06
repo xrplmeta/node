c07a828cfbd4de7dc43f6d56f4c74240c24784c6
Mwni <manuel@tigmar.de>
2022-06-06 06:20:16 +0200

    added meta props

--- src/lib/meta/props.js
+++ src/lib/meta/props.js
@@ -1,13 +1,30 @@
-export async function storeMetas({ meta, metas, issuer, token, nft }){
+export async function write({ meta, props, source, account, token, nft }){
 	let table
 	let subject
 
-	if(issuer){
-		table = 'issuerMetas'
-		subject = { issuer }
+	if(account){
+		table = 'accountProps'
+		subject = { account }
 	}
 
-	for(let [key, value] of Object.entries(metas)){
-		
+	for(let [key, value] of Object.entries(props)){
+		if(value == undefined){
+			await meta[table].delete({
+				where: {
+					account,
+					key,
+					source
+				}
+			})
+		}else{
+			await meta[table].createOne({
+				data: {
+					account,
+					key,
+					value,
+					source
+				}
+			})
+		}
 	}
 }
