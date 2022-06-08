9dbcbd2ca583e2b7e57ef238ee56a441a27001db
Mwni <manuel@tigmar.de>
2022-06-08 05:05:57 +0200

    fixed incorrect var name

--- src/lib/ledger/diff.js
+++ src/lib/ledger/diff.js
@@ -5,9 +5,9 @@ import { write as writeToState } from './write.js'
 export async function advance({ ledger, state }){
 	let affects = extractAffects(ledger)
 	let journal = await state.journal.readOne({ last: true })
-	let newState = { 
-		ledgerIndex: state.ledgerIndex + 1,
-		entriesCount: state.entriesCount,
+	let newJournal = { 
+		ledgerIndex: journal.ledgerIndex + 1,
+		entriesCount: journal.entriesCount,
 		modifiedEntriesCount: 0,
 		creationTime: unixNow()
 	}
@@ -16,21 +16,21 @@ export async function advance({ ledger, state }){
 		for(let { create, modify, remove } of affects){
 			if(create){
 				await writeToState({ state, entry: create, change: 'new' })
-				newState.entriesCount++
+				newJournal.entriesCount++
 			}else if(modify){
 				await writeToState({ state, entry: modify.previous, change: 'deleted' })
 				await writeToState({ state, entry: modify.final, change: 'modified' })
 			}else if(remove){
 				await writeToState({ state, entry: remove, change: 'deleted' })
-				newState.entriesCount--
+				newJournal.entriesCount--
 			}
 
-			newState.modifiedEntriesCount++
+			newJournal.modifiedEntriesCount++
 		}
 
 		await state.journal.createOne({
 			data: {
-				...newState,
+				...newJournal,
 				completionTime: unixNow()
 			}
 		})
