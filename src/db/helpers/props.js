b684cc421034c0f27809202779df874b51c08281
Mwni <manuel@tigmar.de>
2022-07-08 05:18:23 +0200

    wrap prop writes into transaction

--- src/db/helpers/props.js
+++ src/db/helpers/props.js
@@ -1,25 +1,30 @@
 export function writeTokenProps({ ctx, token, props, source }){
-	for(let [key, value] of Object.entries(props)){
-		ctx.db.tokenProps.createOne({
-			data: {
-				token,
-				key,
-				value,
-				source
-			}
-		})
-	}
+	ctx.db.tx(() => {
+		for(let [key, value] of Object.entries(props)){
+			ctx.db.tokenProps.createOne({
+				data: {
+					token,
+					key,
+					value,
+					source
+				}
+			})
+		}
+	})
+	
 }
 
 export function writeAccountProps({ ctx, account, props, source }){
-	for(let [key, value] of Object.entries(props)){
-		ctx.db.accountProps.createOne({
-			data: {
-				account,
-				key,
-				value,
-				source
-			}
-		})
-	}
+	ctx.db.tx(() => {
+		for(let [key, value] of Object.entries(props)){
+			ctx.db.accountProps.createOne({
+				data: {
+					account,
+					key,
+					value,
+					source
+				}
+			})
+		}
+	})
 }
