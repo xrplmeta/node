1cb202993fe3afdd807343360546b6eb0b912918
Mwni <manuel@tigmar.de>
2022-07-09 17:54:48 +0200

    delete null props

--- src/db/helpers/props.js
+++ src/db/helpers/props.js
@@ -1,14 +1,24 @@
 export function writeTokenProps({ ctx, token, props, source }){
 	ctx.db.tx(() => {
 		for(let [key, value] of Object.entries(props)){
-			ctx.db.tokenProps.createOne({
-				data: {
-					token,
-					key,
-					value,
-					source
-				}
-			})
+			if(value == null){
+				ctx.db.tokenProps.deleteOne({
+					where: {
+						token,
+						key,
+						source
+					}
+				})
+			}else{
+				ctx.db.tokenProps.createOne({
+					data: {
+						token,
+						key,
+						value,
+						source
+					}
+				})
+			}
 		}
 	})
 	
@@ -17,14 +27,24 @@ export function writeTokenProps({ ctx, token, props, source }){
 export function writeAccountProps({ ctx, account, props, source }){
 	ctx.db.tx(() => {
 		for(let [key, value] of Object.entries(props)){
-			ctx.db.accountProps.createOne({
-				data: {
-					account,
-					key,
-					value,
-					source
-				}
-			})
+			if(value == null){
+				ctx.db.accountProps.deleteOne({
+					where: {
+						account,
+						key,
+						source
+					}
+				})
+			}else{
+				ctx.db.accountProps.createOne({
+					data: {
+						account,
+						key,
+						value,
+						source
+					}
+				})
+			}
 		}
 	})
 }
