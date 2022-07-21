127b05ab5a595b9ebffad341f59f7b70f9edf3da
Mwni <manuel@tigmar.de>
2022-07-22 00:53:20 +0200

    extended server_info response

--- src/srv/procedures/server.js
+++ src/srv/procedures/server.js
@@ -3,10 +3,21 @@ import { getAvailableRange } from '../../db/helpers/ledgers.js'
 
 
 export function serveServerInfo(){
-	return () => {
+	return ({ ctx }) => {
 		return {
 			server_version: version,
-			available_range: getAvailableRange({ ctx })
+			available_range: getAvailableRange({ ctx }),
+			tokenslists: ctx.config.crawl?.tokenlist
+				? ctx.config.crawl?.tokenlist.map(
+					list => ({
+						id: list.id,
+						url: list.url,
+						trust_level: list.trustLevel
+					})
+				)
+				: [],
+			total_tokens: Number(ctx.db.tokens.count()),
+			total_nfts: 0
 		}
 	}
 }
