5cc479ed42daa7402a14b3d773f8fc19497c49f7
Mwni <manuel@tigmar.de>
2022-07-26 06:17:28 +0200

    add native dependency setup

--- src/app/ledger.js
+++ src/app/ledger.js
@@ -26,6 +26,19 @@ export async function runSnapshot({ ctx }){
 	if(ctx.log)
 		log.pipe(ctx.log)
 
+
+	let db = openDB({ ctx })
+
+	console.log(
+		db.tokenSupply.readManyRaw({
+			query: 
+				`SELECT sumint(value) FROM TokenSupply LIMIT 3`,
+			params: []
+		})
+	)
+
+	await new Promise(r => r)
+
 	return await createSnapshot({
 		ctx: {
 			...ctx,
