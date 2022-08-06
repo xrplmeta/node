f00fbe4051ec822ea5ee912bd523d62a0f5ebb60
Mwni <manuel@tigmar.de>
2022-08-07 00:53:19 +0200

    spawn every crawler in dedicated process
    this reduces cross blocking on database requests

--- src/crawl/crawl.js
+++ src/crawl/crawl.js
@@ -1,23 +1,37 @@
 import log from '@mwni/log'
+import { spawn } from 'multitasked'
+import { open } from '../db/index.js'
 import crawlers from './crawlers/index.js'
 
 
 export async function startCrawlers({ ctx }){
-	let active = {}
+	for(let { name } of crawlers){
+		spawn(':spawnCrawler', { ctx, name })
+	}
+}
 
-	for(let { name, start } of crawlers){
-		active[name] = start({ ctx })
-			.catch(error => {
-				log.warn(`skipping crawler [${name}]:`, error.message)
-				delete active[name]
-			})
+export async function spawnCrawler({ ctx, name }){
+	let { start } = crawlers.find(crawler => crawler.name === name)
+	let crashed = false
+
+	log.pipe(ctx.log)
+
+	ctx = {
+		...ctx,
+		db: open({ ctx })
 	}
 
+	start({ ctx })
+		.catch(error => {
+			log.warn(`skipping crawler [${name}]:`, error.message)
+			crashed = true
+		})
+
 	await Promise.resolve()
-	await Promise.all(Object.entries(active).map(
-		([name, promise]) => {
-			log.info(`started crawler [${name}]`)
-			return promise
-		}
-	))
+
+	if(!crashed){
+		log.info(`started crawler [${name}]`)
+	}else{
+		process.exit()
+	}
 }
