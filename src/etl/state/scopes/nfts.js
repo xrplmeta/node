8352492eaf81bdaa33c25640811be7db69e1ec4a
Mwni <manuel@tigmar.de>
2022-10-26 23:34:46 +0200

    add xls-20 support

--- src/etl/state/scopes/nfts.js
+++ src/etl/state/scopes/nfts.js
@@ -1,6 +1,8 @@
-/*
-export function NFTokenPage({ entry }){
-	let address = encodeAccountID(Buffer.from(entry.index.slice(0, 40), 'hex'))
+import { encodeAccountID } from 'ripple-address-codec'
+
+
+export function parse({ index, entry }){
+	let address = encodeAccountID(Buffer.from(index.slice(0, 40), 'hex'))
 	let page = {
 		account: { address },
 		nfts: []
@@ -8,15 +10,47 @@ export function NFTokenPage({ entry }){
 
 	for(let { NFToken } of entry.NFTokens){
 		let issuer = encodeAccountID(Buffer.from(NFToken.NFTokenID.slice(8, 48), 'hex'))
+		let uri = NFToken.URI
+			? Buffer.from(NFToken.URI, 'hex')
+			: null
 
 		page.nfts.push({
-			account: { address },
+			owner: { address },
 			issuer: { address: issuer },
 			tokenId: NFToken.NFTokenID,
-			uri: NFToken.URI,
+			uri,
 		})
 	}
 
 	return page
 }
-*/
+
+
+
+export function diff({ ctx, previous, final }){
+	if(previous){
+		for(let { owner, ...pNft } of previous.nfts){
+			if(final && final.nfts.some(fNft => fNft.tokenId === pNft.tokenId))
+				continue
+
+			ctx.db.nfts.createOne({
+				data: ctx.backwards
+					? pNft
+					: { ...pNft, owner: null }
+			})
+		}
+	}
+
+	if(final){
+		for(let { owner, ...fNft } of final.nfts){
+			if(previous && previous.nfts.some(pNft => pNft.tokenId === fNft.tokenId))
+				continue
+
+			ctx.db.nfts.createOne({
+				data: ctx.backwards
+					? fNft
+					: { ...fNft, owner }
+			})
+		}
+	}
+}
