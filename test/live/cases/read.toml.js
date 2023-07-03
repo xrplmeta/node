ad662c2827f4c39fe91a8fc77eb68ff6c340344f
Mwni <manuel@tigmar.de>
2023-07-04 00:06:52 +0200

    add live test for xls26 toml parsing

---
+++ test/live/cases/read.toml.js
@@ -0,0 +1,30 @@
+import log from '@mwni/log'
+import { parse as parseXLS26 } from '@xrplkit/xls26'
+import { createFetch } from '../../../src/lib/fetch.js'
+
+
+export default async ({ config, args }) => {
+	let tomlUrl = args._[1]
+	let fetch = createFetch({
+		headers: {
+			'user-agent': config.crawl?.userAgent ||
+				'XRPL-Meta-Crawler (https://xrplmeta.org)'
+		}
+	})
+
+	if(!tomlUrl){
+		throw new Error(`no toml url provided. use: npm livetest read.toml [url]`)
+	}
+
+	log.info(`fetching ${tomlUrl}`)
+
+	let { status, data } = await fetch(tomlUrl)
+	
+	if(status !== 200){
+		throw new Error(`HTTP ${status}: ${tomlUrl}`)
+	}
+
+	let xls26 = parseXLS26(data)
+
+	log.info(`parsed xls26:\n`, xls26)
+}
