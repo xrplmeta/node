8a2a1f4b36b38c5c2dedbdb766d5eff7abcfcd90
Mwni <manuel@tigmar.de>
2023-11-29 21:43:11 +0100

    add --only-server flag

--- src/app/main.js
+++ src/app/main.js
@@ -6,35 +6,38 @@ import { run as runServerApp } from './server.js'
 import createIPC from '../lib/ipc.js'
 
 
-export default async function({ config }){
+export default async function({ config, args }){
 	const ctx = {
 		ipc: createIPC(),
 		config,
 		log,
 	}
 
-	await runLedgerApp({ ctx })
-		.catch(error => {
-			log.error(`ledger app crashed due to fatal error:`)
-			log.error(error)
-			process.exit(1)
-		})
 
-	log.info(`bootstrap complete`)
-	
-	runCrawlApp({ ctx })
-		.catch(error => {
-			log.error(`crawl app crashed due to fatal error:`)
-			log.error(error)
-			log.warn(`attempting to continue without it`)
-		})
+	if(!args['only-server']){
+		await runLedgerApp({ ctx })
+			.catch(error => {
+				log.error(`ledger app crashed due to fatal error:`)
+				log.error(error)
+				process.exit(1)
+			})
 
-	runCacheApp({ ctx })
-		.catch(error => {
-			log.error(`cache app crashed due to fatal error:`)
-			log.error(error)
-			log.warn(`attempting to continue without it`)
-		})
+		log.info(`bootstrap complete`)
+		
+		runCrawlApp({ ctx })
+			.catch(error => {
+				log.error(`crawl app crashed due to fatal error:`)
+				log.error(error)
+				log.warn(`attempting to continue without it`)
+			})
+
+		runCacheApp({ ctx })
+			.catch(error => {
+				log.error(`cache app crashed due to fatal error:`)
+				log.error(error)
+				log.warn(`attempting to continue without it`)
+			})
+	}
 
 	runServerApp({ ctx })
 		.catch(error => {
