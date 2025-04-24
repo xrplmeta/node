11311f26767ec22a0ce04930836dda4f9666f7dd
Mwni <manuel@tigmar.de>
2025-04-24 17:21:59 +0200

    drop xrplf self-assessment crawler

--- src/crawl/crawlers/index.js
+++ src/crawl/crawlers/index.js
@@ -1,7 +1,6 @@
 import domains from './domains.js'
 import tokenlists from './tokenlists.js'
 import xumm from './xumm.js'
-import xrplf from './xrplf.js'
 import bithomp from './bithomp.js'
 import xrpscan from './xrpscan.js'
 import gravatar from './gravatar.js'
@@ -11,7 +10,6 @@ export default [
 	{ name: 'domains', start: domains },
 	{ name: 'tokenlists', start: tokenlists },
 	{ name: 'xumm', start: xumm },
-	{ name: 'xrplf', start: xrplf },
 	{ name: 'bithomp', start: bithomp },
 	{ name: 'xrpscan', start: xrpscan },
 	{ name: 'gravatar', start: gravatar },
