0ca20f2a0eded4efb06bb23fe9faea6242a71a04
Mwni <manuel@tigmar.de>
2025-10-10 20:45:56 +0200

    rename etl to ledger

--- src/app/ledger.js
+++ src/app/ledger.js
@@ -2,9 +2,9 @@ import log from '@mwni/log'
 import { spawn } from '@mwni/workers'
 import { createPool } from '../xrpl/nodepool.js'
 import { openDB } from '../db/index.js'
-import { createSnapshot } from '../etl/snapshot.js'
-import { startSync } from '../etl/sync.js'
-import { startBackfill } from '../etl/backfill.js'
+import { createSnapshot } from '../ledger/snapshot.js'
+import { startSync } from '../ledger/sync.js'
+import { startBackfill } from '../ledger/backfill.js'
 
 
 export async function run({ ctx }){
