cd6c493786c8968d2c32d3419c34309e0c54a097
Raj Patel <rajp@ripple.com>
2025-11-27 21:28:27 -0500

    keep last updated metadata

--- src/ledger/state/index.js
+++ src/ledger/state/index.js
@@ -33,7 +33,8 @@ export function applyLedgerStateFromObjects({ ctx, objects }){
 export function applyLedgerStateFromTransactions({ ctx, ledger }){
 	let deltas = []
 
-	for(let transaction of ledger.transactions){
+	for(let i = 0; i < ledger.transactions.length; i++){
+		let transaction = ledger.transactions[i]
 		let meta = transaction.meta || transaction.metaData
 
 		for(let { CreatedNode, ModifiedNode, DeletedNode } of meta.AffectedNodes){
@@ -41,6 +42,8 @@ export function applyLedgerStateFromTransactions({ ctx, ledger }){
 				deltas.push({
 					type: CreatedNode.LedgerEntryType,
 					index: CreatedNode.LedgerIndex,
+					ledgerSequence: ledger.sequence,
+					transactionIndex: i,
 					final: {
 						...CreatedNode.NewFields,
 						LedgerSequence: ledger.sequence
@@ -58,6 +61,8 @@ export function applyLedgerStateFromTransactions({ ctx, ledger }){
 				deltas.push({
 					type: ModifiedNode.LedgerEntryType,
 					index: ModifiedNode.LedgerIndex,
+					ledgerSequence: ledger.sequence,
+					transactionIndex: i,
 					previous: {
 						...ModifiedNode.FinalFields,
 						...ModifiedNode.PreviousFields,
@@ -72,6 +77,8 @@ export function applyLedgerStateFromTransactions({ ctx, ledger }){
 				deltas.push({
 					type: DeletedNode.LedgerEntryType,
 					index: DeletedNode.LedgerIndex,
+					ledgerSequence: ledger.sequence,
+					transactionIndex: i,
 					previous: {
 						...DeletedNode.FinalFields,
 						...DeletedNode.PreviousFields,
@@ -86,7 +93,7 @@ export function applyLedgerStateFromTransactions({ ctx, ledger }){
 		return applyDeltas({
 			ctx,
 			deltas: deltas
-				.map(({ type, index, previous, final }) => ({ type, index, previous: final, final: previous }))
+				.map(({ type, index, ledgerSequence, transactionIndex, previous, final }) => ({ type, index, ledgerSequence, transactionIndex, previous: final, final: previous }))
 				.reverse(),
 		})
 	}else{
@@ -101,7 +108,7 @@ function applyDeltas({ ctx, deltas }){
 	let groups = {}
 	let solos = []
 
-	for(let { type, index, previous, final } of deltas){
+	for(let { type, index, ledgerSequence, transactionIndex, previous, final } of deltas){
 		let module = ledgerEntryModules[type]
 
 		if(!module)
@@ -143,6 +150,8 @@ function applyDeltas({ ctx, deltas }){
 		}else{
 			solos.push({
 				type,
+				ledgerSequence,
+				transactionIndex,
 				previous: parsedPrevious, 
 				final: parsedFinal 
 			})
--- src/ledger/state/mptokenissuance.js
+++ src/ledger/state/mptokenissuance.js
@@ -1,7 +1,7 @@
-import { parse as parseXLS89 } from '@xrplkit/xls89'
+import { clearTokenProps, writeAccountProps, writeTokenProps } from "../../db/helpers/props.js"
 import { mptIssuanceIdFromIssuerAndSequence } from "../../xrpl/mpt.js"
 import TokenType from "../../xrpl/tokentype.js"
-import { clearTokenProps, writeTokenProps } from '../../db/helpers/props.js'
+import { parse as parseXLS89 } from '@xrplkit/xls89'
 
 export function parse({ entry }){
     return {
@@ -11,7 +11,7 @@ export function parse({ entry }){
     }
 }
 
-export function diff({ ctx, previous, final }){
+export function diff({ ctx, ledgerSequence, transactionIndex, previous, final }){
     let issuer = final?.issuer || previous?.issuer
     let sequence = final?.sequence || previous?.sequence
 
@@ -22,23 +22,86 @@ export function diff({ ctx, previous, final }){
         mptIssuanceId: mptIssuanceIdFromIssuerAndSequence(issuer, sequence),
         tokenType: TokenType.MPT
     }
-    ctx.db.core.tokens.createOne({
+    token = ctx.db.core.tokens.createOne({
         data: token
     })
 
-    if (ctx.backward){
-        if (final && !previous){
-            updateTokenProps({ctx, token, metadata: final.mptokenMetadata})
+    let row = ctx.db.core.mptokenMetadataUpdates.createOne({
+        data: {
+            token
         }
-    }else{
-        if (final && final?.mptokenMetadata != previous?.mptokenMetadata){
-            updateTokenProps({ctx, token, metadata: final.mptokenMetadata})
+    })
+
+    // Parse and persist metadata when applying ledger state
+    if (ledgerSequence == null || transactionIndex == null) {
+        updateTokenAndAccountProps({ctx, token, mptokenMetadata: final.mptokenMetadata, overwriteAccountProps: true})
+        return
+    }
+
+    let isDeleted = ctx.backwards ? final && !previous : previous && !final
+    let adjustIssuerNameProp = isDeleted;
+    if (!isDeleted && final?.mptokenMetadata != previous?.mptokenMetadata){
+        if (!ctx.backwards) {
+            updateTokenAndAccountProps({ctx, token, mptokenMetadata: final.mptokenMetadata, overwriteAccountProps: false})
+            adjustIssuerNameProp = true
         }
+        
+        // Consider only the latest update to metadata during backfill phase
+        if (ctx.backwards && row.ledgerSequence == null && row.transactionIndex == null){
+            adjustIssuerNameProp = true
+        }
+        if (ledgerSequence >= row.ledgerSequence && transactionIndex > row.transactionIndex){
+            row.ledgerSequence = ledgerSequence
+            row.transactionIndex = transactionIndex
+        }
+        row.ledgerSequence = row.ledgerSequence ?? ledgerSequence
+        row.transactionIndex = row.transactionIndex ?? transactionIndex
+    }
+    ctx.db.core.mptokenMetadataUpdates.updateOne({
+        data: {
+            ledgerSequence: row.ledgerSequence,
+            transactionIndex: row.transactionIndex,
+            deleted: isDeleted ? true: row.deleted
+        },
+        where: {
+            id: row.id
+        }
+    })
+
+    if (adjustIssuerNameProp) {
+        let rows = ctx.db.core.mptokenMetadataUpdates.readManyRaw({
+			query: 
+				`select
+                    tp.value, mmu.ledgerSequence
+                from
+                    MPTokenMetadataUpdate mmu
+                inner join Token t
+                inner join Account a
+                inner join TokenProp tp
+                where
+                    t.issuer = a.id
+                    and t.id = mmu.token
+                    and t.id = tp.token
+                    and tp.key = 'issuer_name'
+                    and tp."source" = 'ledger'
+                    and mmu.deleted = false
+                    and a.id = ?
+                order by
+                    mmu.ledgerSequence desc,
+                    mmu.transactionIndex desc;`,
+			params: [
+				token.issuer.id
+			]
+		})
+        let rowsWithKnownOrder = rows.filter(row => row.ledgerSequence != null)
+        let rowsWithUnknownOrder = rows.filter(row => row.ledgerSequence == null)
+        let issuerName = rowsWithKnownOrder.length > 0 ? rowsWithKnownOrder[0].value : (rowsWithUnknownOrder.length > 0 ? rowsWithUnknownOrder[0].value : null)
+        writeAccountProps({ctx, account: token.issuer, props: {name: issuerName}, source: 'ledger'})
     }
 }
 
-function updateTokenProps({ctx, token, metadata}){
-    let {token: props} = parseXLS89(metadata)
+function updateTokenAndAccountProps({ctx, token, mptokenMetadata, overwriteAccountProps}) {
+    let {token: props} = parseXLS89(mptokenMetadata)
     clearTokenProps({
         ctx,
         token,
@@ -50,4 +113,6 @@ function updateTokenProps({ctx, token, metadata}){
         props,
         source: 'ledger'
     })
+    if (overwriteAccountProps && Object.keys(props) != 0)
+        writeAccountProps({ctx, account: token.issuer, props: {name: props.issuer_name}, source: 'ledger'})
 }
